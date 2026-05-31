a08043ebb1827fcb50453197f9ebf6640bc5d48d
Antonio <antperdev+github@gmail.com>
2026-05-31 06:33:59 +0200

    Revert "Add security headers & deployment workflow"
    This reverts commit 48c107ce07ca64e2034a658e1a1edcc953029587.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://antperdev.github.io/miportfolio',
-  base: '/miportfolio/',
+  site: 'https://antperdev.github.io', // Cambiar por tu URL de despliegue real
 });
