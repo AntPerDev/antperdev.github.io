07e8f37682a41a61144f889ec594f1b2c4e9d7e2
AntPerDev <antperdev+github@gmail.com>
2026-09-03 17:54:44 +0200

    fix: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   // URL completa del sitio en GitHub Pages (repo: antperdev.github.io)
-  site: 'https://antperdev.github.io',
+  site: "https://antperdev.github.io",
   // Ruta base para que los enlaces se generen en la raíz del dominio
   // base: '/docs',
   // Asegura que los archivos estáticos tengan barra final y se sirvan correctamente
-  // trailingSlash: 'always',
+  base: "/",
+  trailingSlash: "always",
 });
